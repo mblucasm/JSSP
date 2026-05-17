c068ffffc296b668b32d6f2b6c18146441eff915
Lucas Martin <mblucasm@gmail.com>
2026-05-17 16:35:28 +0200

    added models to todot

--- todot.py
+++ todot.py
@@ -1,14 +1,18 @@
 import os
 import sys
 
-import src.models.cp
-import src.models.pyomo
+import src.models.cp as cp
+import src.models.pyomo as pyomo
+import src.models.ais as ais
+import src.models.ts as ts
+import src.models.sa as sa
+import src.models.icg as icg
 
 from src.shared import Instance
 
 class Program:
 
-    MODELS = ["TI-HiGHS", "TI-MOSEK", "DI-HiGHS", "DI-MOSEK", "CP"]
+    MODELS = ["TI-HiGHS", "TI-MOSEK", "DI-HiGHS", "DI-MOSEK", "CP", "AIS", "TS", "SA", "ICG-HiGHS", "ICG-MOSEK"]
 
     def __init__(self, argv: list[str]) -> None:
 
@@ -48,22 +52,33 @@ def main(argv: list[str]) -> None:
 
     result = None
     if program.model:
-        assert len(program.MODELS) == 5
+        assert len(program.MODELS) == 10
         if program.model == "TI-HiGHS":
-            model = src.models.pyomo.TimeIndex(instance)
+            model = pyomo.TimeIndex(instance)
             result = model.solve("appsi_highs", tee = True)
         elif program.model == "TI-MOSEK":
-            model = src.models.pyomo.TimeIndex(instance)
+            model = pyomo.TimeIndex(instance)
             result = model.solve("mosek", tee = True)
         elif program.model == "DI-HiGHS":
-            model = src.models.pyomo.Disjunctive(instance)
+            model = pyomo.Disjunctive(instance)
             result = model.solve("appsi_highs", tee = True)
         elif program.model == "DI-MOSEK":
-            model = src.models.pyomo.Disjunctive(instance)
+            model = pyomo.Disjunctive(instance)
             result = model.solve("mosek", tee = True)
         elif program.model == "CP":
-            model = src.models.cp.Disjunctive(instance)
+            model = cp.Disjunctive(instance)
             result = model.solve(tee = True)
+        elif program.model == "AIS":
+            model = ais.AIS(instance)
+            result = model.solve(10000, 3, 'C')
+        elif program.model == "TS":
+            result = ts.solve(instance, 100000)
+        elif program.model == "SA":
+            result = sa.solve(instance, use_bidir = False)
+        elif program.model == "ICG-HiGHS":
+            result = icg.solve(instance, 3600, "appsi_highs")
+        elif program.model == "ICG-MOSEK":
+            result = icg.solve(instance, 3600, "mosek")
         else:
             raise RuntimeError("UNREACHABLE")
 
